35000e9b210ffb4ad6ee3dd97071b5d5357ede44
roryu <rulloa1@my.devry.edu>
2025-09-26 16:04:22 -0500

    Update buy buttons to redirect to payment page and add placeholder images

---
+++ buy-button-script.js
@@ -0,0 +1,14 @@
+<script>
+function buyProduct(productTitle, productPrice, productBalance, productDescription) {
+    // Store product details in sessionStorage for the payment page
+    sessionStorage.setItem('selectedProduct', JSON.stringify({
+        title: productTitle,
+        price: productPrice,
+        balance: productBalance,
+        description: productDescription
+    }));
+    
+    // Redirect to payment/topup page
+    window.location.href = '../../show_order.html';
+}
+</script>
